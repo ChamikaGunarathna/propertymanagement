f7d28250e4018f41d722e17b2f7c8925c99c6d6d
Chamika <cdb.gunarathna@gmail.com>
2022-11-18 17:35:05 +0530

    implemented the field validation exception handling

--- src/main/java/com/mycompany/propertymanagement/exception/CustomExceptionHandler.java
+++ src/main/java/com/mycompany/propertymanagement/exception/CustomExceptionHandler.java
@@ -2,13 +2,31 @@ package com.mycompany.propertymanagement.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @ControllerAdvice
 public class CustomExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<ErrorModel>> handleFieldException(MethodArgumentNotValidException manv){
+        List<ErrorModel> errorModelList = new ArrayList<>();
+        ErrorModel error = null;
+        List<FieldError> fieldErrorList = manv.getBindingResult().getFieldErrors();
+        for (FieldError fe:fieldErrorList){
+            error = new ErrorModel();
+            error.setCode(fe.getField());
+            error.setErrorMessage(fe.getDefaultMessage());
+            errorModelList.add(error);
+        }
+
+        return new ResponseEntity<List<ErrorModel>>(errorModelList, HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<List<ErrorModel>> handleBusinessException(BusinessException bex){
